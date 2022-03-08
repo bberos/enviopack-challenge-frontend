a8098ab6fdcd61e0cee3a21e5bfcb3498a4c4de0
bberos <bberos.171@gmail.com>
2022-03-08 01:21:21 -0300

    styles(Cart/Total): layout

---
+++ src/components/Cart/TotalCart/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 65%;
+  flex-direction: row;
+  margin-top: 20px;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #b6b6b6;
+  border-radius: 5px;
+  padding: 15px;
+  height: 4vh;
+  box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+  justify-content: space-between;
+  -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+  -moz-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+  h1 {
+    font-size: 1.4rem;
+    font-weight: bold;
+    margin-left: 33.5px;
+    margin-right: 33.5px;
+  }
+`;
