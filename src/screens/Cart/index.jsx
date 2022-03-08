cd0b7c867802fd0f0f99f1b03d656d0be3aa081d
bberos <bberos.171@gmail.com>
2022-03-08 01:21:40 -0300

    feat(CartScreen): import components layouts

--- src/screens/Cart/index.jsx
+++ src/screens/Cart/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { Container } from "./styles";
+import CartList from "./../../components/Cart/CartList";
+import TotalCart from "./../../components/Cart/TotalCart";
 
 export default function CartScreen() {
-  return <Container>CartScreen</Container>;
+  return (
+    <Container>
+      <h1>Carrito</h1>
+      <CartList />
+      <TotalCart />
+      <div className="ButtonsContainer">
+        <button>Volver al catalogo</button>
+        <button>Finalizar compra</button>
+      </div>
+    </Container>
+  );
 }
