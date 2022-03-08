44aedc778c93aeb96f325d1bee1bb8e3f7ae50ab
bberos <bberos.171@gmail.com>
2022-03-08 00:35:00 -0300

    styles(ShopScreen): filterContainer & align

--- src/screens/Shop/styles.js
+++ src/screens/Shop/styles.js
@@ -3,4 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  .filtersContainer {
+    width: 75%;
+    display: flex;
+    height: 10vh;
+  }
 `;
