61a417f24725e094913902ffe2467782a9f0c3e9
bberos <bberos.171@gmail.com>
2022-03-08 20:12:04 -0300

    feat(TotalCart): total prop

--- src/components/Cart/TotalCart/index.jsx
+++ src/components/Cart/TotalCart/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container } from "./styles";
 
-export default function TotalCart() {
+export default function TotalCart({ total }) {
   return (
     <Container>
       <h1>Total</h1>
-      <h1>$21499</h1>
+      <h1>${total}</h1>
     </Container>
   );
 }
