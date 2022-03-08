89d79c8274f2a825a0c096946aadb8998d8d42b1
bberos <bberos.171@gmail.com>
2022-03-08 20:07:57 -0300

    feat(CartList): dynamic cartProduct object

--- src/components/Cart/CartList/index.jsx
+++ src/components/Cart/CartList/index.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Container } from "./styles";
 import CartProduct from "./../CartProduct";
 
-export default function CartList() {
+export default function CartList(props) {
+  const { cartProduct } = props;
   return (
     <Container>
-      <CartProduct />
-      <CartProduct />
-      <CartProduct />
-      <CartProduct />
+      {cartProduct ? (
+        cartProduct.map((product) => {
+          return <CartProduct key={product.id} product={product} />;
+        })
+      ) : (
+        <h1>No hay productos en el carrito.</h1>
+      )}
     </Container>
   );
 }
