8b81da066faa46f09ddf4a3c0b5039bdf30d6f98
bberos <bberos.171@gmail.com>
2022-03-08 00:30:25 -0300

    styles(FilterByProductName): base styles

---
+++ src/components/FilterProductName/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  justify-content: center;
+`;
+
+export const InputProduct = styled.input`
+  width: 15rem;
+  height: 2rem;
+  background-color: #f2f2f2;
+  border: 1px solid #d0d3d9;
+  border-radius: 2.5px;
+  outline: none;
+  padding-left: 5px;
+`;
