a31725e07323da38e9a6539c19139195aff31192
bberos <bberos.171@gmail.com>
2022-03-08 20:06:46 -0300

    styles(Button): shared styled button component

--- src/styles/shared/Button.js
+++ src/styles/shared/Button.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  width: 80%;
+  width: ${(props) => props.width || "100%"};
   height: 2rem;
   cursor: pointer;
-  background-color: #212221;
+  background-color: ${(props) => props.background || "#212221"};
+  background-color: ${(props) => (props.disabled ? "#d3d3d3" : "#212221")};
   border: none;
   border-radius: 5px;
-  color: #eef4f5;
+  color: ${(props) => (props.disabled ? "#212221" : "#eef4f5")};
   letter-spacing: 0.5px;
   transition: all 0.4s;
   &:hover {
-    background-color: #000;
+    background-color: ${(props) => (props.disabled ? "#d3d3d3" : "#000")}#000;
   }
 `;
