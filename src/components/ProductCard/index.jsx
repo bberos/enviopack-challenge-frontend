1bbfd2f3c534e7670d3ed0befee900fc029a1636
bberos <bberos.171@gmail.com>
2022-03-08 20:13:30 -0300

    feat(Shop/ProductCard): Product card add to cart funct

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { Container, Button } from "./styles";
+import { Container } from "./styles";
+import { Button } from "./../../styles/shared/Button";
+import { useCartProducts } from "../../context/CartProvider";
+import { useNavigate } from "react-router-dom";
 
-export default function ProductCard(props) {
-  const { productData } = props;
+export default function ProductCard({ productData }) {
+  const { id, title, price } = productData;
+  const navigateTo = useNavigate();
+  const { cartProductId, storeCartProductId } = useCartProducts();
+
+  const checkCart = () => {
+    const result = cartProductId.some((value) => value === id);
+    return result;
+  };
+  const handleAddToCart = () => {
+    if (checkCart()) {
+      return navigateTo("/cart");
+    }
+    return storeCartProductId(id);
+  };
   return (
     <Container>
       <img
@@ -10,9 +26,11 @@ export default function ProductCard(props) {
         alt="product-image"
         className="image"
       />
-      <h1 className="title">{productData.title}</h1>
-      <h1 className="price">$ {productData.price}</h1>
-      <Button>Agregar al carrito</Button>
+      <h1 className="title">{title}</h1>
+      <h1 className="price">$ {price}</h1>
+      <Button width={"80%"} onClick={() => handleAddToCart()}>
+        {checkCart() ? "Ver Carrito" : "Agregar al carrito"}
+      </Button>
     </Container>
   );
 }
