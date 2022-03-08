5cd819222dcee2bc368c98c38993fbed44940ba3
bberos <bberos.171@gmail.com>
2022-03-08 20:13:45 -0300

    styles(ProductCard): remove Button styles

--- src/components/ProductCard/styles.js
+++ src/components/ProductCard/styles.js
@@ -25,17 +25,3 @@ export const Container = styled.div`
     font-size: 1.25rem;
   }
 `;
-export const Button = styled.button`
-  width: 80%;
-  height: 2rem;
-  cursor: pointer;
-  background-color: #212221;
-  border: none;
-  border-radius: 5px;
-  color: #eef4f5;
-  letter-spacing: 0.5px;
-  transition: all 0.4s;
-  &:hover {
-    background-color: #000;
-  }
-`;
