daf6fa760c44631de226129fe47779abef485a21
bberos <bberos.171@gmail.com>
2022-03-08 20:09:06 -0300

    styles(CartScreen): buttonsContainer

--- src/screens/Cart/styles.js
+++ src/screens/Cart/styles.js
@@ -4,4 +4,12 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  align-self: center;
+  width: 65%;
+  .buttonsContainer {
+    display: flex;
+    width: 100%;
+    margin-top: 20px;
+    justify-content: space-between;
+  }
 `;
