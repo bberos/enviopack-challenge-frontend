4277ea677d40ffb5adce99f8b9758f90e8d409fa
bberos <bberos.171@gmail.com>
2022-03-08 01:22:13 -0300

    feat(Button): to do styled shared component

---
+++ src/styles/shared/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 80%;
+  height: 2rem;
+  cursor: pointer;
+  background-color: #212221;
+  border: none;
+  border-radius: 5px;
+  color: #eef4f5;
+  letter-spacing: 0.5px;
+  transition: all 0.4s;
+  &:hover {
+    background-color: #000;
+  }
+`;
