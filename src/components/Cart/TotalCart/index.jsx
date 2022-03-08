81c9f5ddc95d2e03bc15140d883303258910872a
bberos <bberos.171@gmail.com>
2022-03-08 01:21:15 -0300

    feat(Cart/Total): layout

---
+++ src/components/Cart/TotalCart/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default function TotalCart() {
+  return (
+    <Container>
+      <h1>Total</h1>
+      <h1>$21499</h1>
+    </Container>
+  );
+}
