66662a45a343b55161a0e42f2b856b65f6e8d04b
bberos <bberos.171@gmail.com>
2022-03-08 00:35:42 -0300

    feat(ShopScreen): render and filter funcionality | to refactor |

--- src/screens/Shop/index.jsx
+++ src/screens/Shop/index.jsx
@@ -1,6 +1,108 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import FilterProductName from "../../components/FilterProductName";
+import OrderProductByPrice from "../../components/OrderProductByPrice";
 import { Container } from "./styles";
+import products from "./../../assets/json/products.json";
+import ProductList from "../../components/ProductsList";
+import Paginate from "../../components/Paginate";
 
 export default function ShopScreen() {
-  return <Container>Shop Screen</Container>;
+  const [productName, setProductName] = useState("");
+  const [selectedValue, setSelectedValue] = useState("default");
+  const [productsList, setProductsList] = useState(products.productos);
+
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [filterProductList, setFilterProductList] = useState();
+
+  //definimos la cantidad de elementos que queremos devolver por pagina.
+  const itemsPerPage = 6;
+
+  //este useEffect nos sirve para el paginado sin filtros y en el caso de que haya filtros tanto por precio como por name.
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    if (selectedValue === "default") {
+      setPageCount(Math.ceil(filterProductList.length / itemsPerPage));
+      setCurrentItems(filterProductList.slice(itemOffset, endOffset));
+    } else {
+      setCurrentItems(sortedItems.slice(itemOffset, endOffset));
+      setPageCount(Math.ceil(sortedItems.length / itemsPerPage));
+    }
+  }, [itemOffset, itemsPerPage, productName, selectedValue]);
+
+  //esta funcion filtra los resultados por el nombre si se ingresa mas de 2 caracteres, lo ideal si le estuviese pegando a una api seria hacer un debounce para no hacer una query por letra.
+  const filterByName = useMemo(() => {
+    let result = [...productsList];
+    if (productName.length < 2) {
+      setFilterProductList(result);
+    } else {
+      let filter = productsList.filter((item) => {
+        return item.title.toLowerCase().includes(productName.toLowerCase());
+      });
+      setFilterProductList(filter);
+    }
+    return result;
+  }, [productName]);
+
+  // esta funcion nos ordena por precio nuestro listado, en el caso de que haya una busqueda por name consume de ese array y en el caso de que no haya un filtro por nombre consume derecho del json "api".
+  const sortedItems = useMemo(() => {
+    let result =
+      productName.length > 1 ? [...filterProductList] : [...productsList];
+    if (selectedValue === "max") {
+      result.sort((a, b) => {
+        if (a.price > b.price) return -1;
+        if (b.price > a.price) return 1;
+        return 0;
+      });
+    }
+    if (selectedValue === "min") {
+      result.sort((a, b) => {
+        if (a.price > b.price) return 1;
+        if (b.price > a.price) return -1;
+        return 0;
+      });
+    }
+    return result;
+  }, [selectedValue, productsList, productName]);
+
+  return (
+    <Container>
+      <h1>Catalogo</h1>
+      <div className="filtersContainer">
+        <FilterProductName
+          productName={productName}
+          setProductName={setProductName}
+        />
+        <OrderProductByPrice
+          selectedValue={selectedValue}
+          setSelectedValue={setSelectedValue}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {currentItems !== null ? (
+          <>
+            <ProductList
+              currentItems={currentItems}
+              itemsPerPage={itemsPerPage}
+            />
+            <Paginate
+              pageCount={pageCount}
+              itemsPerPage={itemsPerPage}
+              productsList={productsList}
+              setItemOffset={setItemOffset}
+            />
+          </>
+        ) : (
+          <h1>Cargando...</h1>
+        )}
+      </div>
+    </Container>
+  );
 }
