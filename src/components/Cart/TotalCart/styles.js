d8bfba27d45b991bf784232dc684d42684c7a274
bberos <bberos.171@gmail.com>
2022-03-08 20:08:36 -0300

    styles(TotalCart): width fix

--- src/components/Cart/TotalCart/styles.js
+++ src/components/Cart/TotalCart/styles.js
@@ -2,15 +2,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  width: 65%;
+  width: 100%;
   flex-direction: row;
   margin-top: 20px;
   align-items: center;
   background-color: #fff;
   border: 1px solid #b6b6b6;
   border-radius: 5px;
-  padding: 15px;
-  height: 4vh;
+  height: 5vh;
   box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
   justify-content: space-between;
   -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
