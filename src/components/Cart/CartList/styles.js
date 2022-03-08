2c97dd192aa8182cad076485ebe85ab0aea60675
bberos <bberos.171@gmail.com>
2022-03-08 20:08:19 -0300

    styles(CartList): width fix

--- src/components/Cart/CartList/styles.js
+++ src/components/Cart/CartList/styles.js
@@ -2,13 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  width: 65%;
+  width: 100%;
   flex-direction: column;
   align-items: center;
   background-color: #fff;
   border: 1px solid #b6b6b6;
   border-radius: 5px;
-  padding: 15px;
   box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
   -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
   -moz-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
