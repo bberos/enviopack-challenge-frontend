e3571b260f5af41ef4247605c0dc860fce5290ef
bberos <bberos.171@gmail.com>
2022-03-09 03:38:35 -0300

    styles(PurchaseScreen): purchase screen styles

--- src/screens/Purchase/styles.js
+++ src/screens/Purchase/styles.js
@@ -3,4 +3,30 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  align-self: center;
+  width: 65%;
+  .purchaseStatusOverlay {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff;
+    border: 1px solid #b6b6b6;
+    border-radius: 5px;
+    box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    -moz-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    margin-top: 15px;
+
+    .purchaseStatusContainer {
+      padding: 15px;
+      h2 {
+        font-size: 1.2rem;
+      }
+      button {
+        margin-top: 10px;
+      }
+    }
+  }
 `;
