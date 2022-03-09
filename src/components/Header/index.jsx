6d079a27a8c7acc7261d3aaf80c8d66da5669a92
bberos <bberos.171@gmail.com>
2022-03-09 03:14:24 -0300

    feat(Header): loader

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,46 @@
 import React, { useMemo } from "react";
 import { NavLink } from "react-router-dom";
 import { useCartProducts } from "../../context/CartProvider";
+import { useUserData } from "../../context/UserProvider";
 import { Container, LeftContainer, RightContainer } from "./styles";
 export default function Header() {
   const { cartProductId } = useCartProducts();
+  const { userData } = useUserData();
 
   const countCartProduct = useMemo(() => {
     return cartProductId?.length;
   }, [cartProductId]);
 
   return (
-    <Container>
-      <LeftContainer>
-        <NavLink
-          to="/"
-          className={(navData) => (navData.isActive ? "link__active" : "link")}
-        >
-          Tienda de Productos
-        </NavLink>
-      </LeftContainer>
-      <RightContainer>
-        <div className="link">Juan Ignacio</div>
-        <NavLink
-          to="/cart"
-          className={(navData) => (navData.isActive ? "link__active" : "link")}
-        >
-          Carrito <>({countCartProduct})</>
-        </NavLink>
-        <div className="link">Credito $50000</div>
-      </RightContainer>
-    </Container>
+    <>
+      {userData === undefined ? (
+        <h1>Cargando...</h1>
+      ) : (
+        <Container>
+          <LeftContainer>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? "link__active" : "link"
+              }
+            >
+              Tienda de Productos
+            </NavLink>
+          </LeftContainer>
+          <RightContainer>
+            <div className="link">{userData.firstName}</div>
+            <NavLink
+              to="/cart"
+              className={(navData) =>
+                navData.isActive ? "link__active" : "link"
+              }
+            >
+              Carrito <>({countCartProduct})</>
+            </NavLink>
+            <div className="link">Credito ${userData.credit}</div>
+          </RightContainer>
+        </Container>
+      )}
+    </>
   );
 }
