c37822ec5cfdfb08997f468f7c7b43b2f9a10cc5
bberos <bberos.171@gmail.com>
2022-03-09 04:38:00 -0300

    styles(Shop/ProductCard): laptop & desktop media-queries

--- src/components/Shop/ProductCard/styles.js
+++ src/components/Shop/ProductCard/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { media } from "./../../../styles/media";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #fff;
-  width: 20rem;
+  width: 15rem;
   margin-bottom: 15px;
   border: 1px solid #b6b6b6;
   border-radius: 5px;
@@ -13,6 +14,12 @@ export const Container = styled.div`
   box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
   -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
   -moz-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+  ${media.desktop} {
+    width: 20rem;
+  }
+  ${media.desktopL} {
+    width: 33rem;
+  }
 
   .image {
     width: 15vw;
