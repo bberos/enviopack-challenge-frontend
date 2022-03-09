e842ed129f6c041936e1be0ab6a16c7145ba8779
bberos <bberos.171@gmail.com>
2022-03-09 03:37:54 -0300

    styles(CartScreen): card-overlay

--- src/screens/Cart/styles.js
+++ src/screens/Cart/styles.js
@@ -6,6 +6,19 @@ export const Container = styled.div`
   align-items: center;
   align-self: center;
   width: 65%;
+  .cardOverlay {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff;
+    border: 1px solid #b6b6b6;
+    border-radius: 5px;
+    box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    -webkit-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    -moz-box-shadow: 2px 1px 8px 1px rgba(0, 0, 0, 0.44);
+    margin-top: 15px;
+  }
   .buttonsContainer {
     display: flex;
     width: 100%;
