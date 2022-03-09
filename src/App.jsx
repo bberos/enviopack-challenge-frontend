4020ad62aeec9db0d9e090f5a3320611707f8877
bberos <bberos.171@gmail.com>
2022-03-09 03:13:09 -0300

    feat(App): UserProvider wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import CartProvider from "./context/CartProvider";
+import UserProvider from "./context/UserProvider";
 
 import MyRoutes from "./MyRoutes";
 
 function App() {
   return (
     <CartProvider>
-      <div className="App">
-        <Header />
-        <MyRoutes />
-      </div>
+      <UserProvider>
+        <div className="App">
+          <Header />
+          <MyRoutes />
+        </div>
+      </UserProvider>
     </CartProvider>
   );
 }
