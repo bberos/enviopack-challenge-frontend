242bd95e2e0f8ed342674308354e40c077eb909e
bberos <bberos.171@gmail.com>
2022-03-09 03:15:31 -0300

    feat(Purchase): layout

--- src/screens/Purchase/index.jsx
+++ src/screens/Purchase/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container } from "./styles";
+import { useLocation } from "react-router-dom";
 
 export default function PurchaseScreen() {
+  const location = useLocation();
+  console.log(location.state);
   return <Container>PurchaseScreen</Container>;
 }
