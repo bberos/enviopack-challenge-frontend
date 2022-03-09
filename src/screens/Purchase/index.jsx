815d74e23a93011d0b524f275fdf7f74423b6bf7
bberos <bberos.171@gmail.com>
2022-03-09 03:38:18 -0300

    feat(PurchaseScreen): final screen

--- src/screens/Purchase/index.jsx
+++ src/screens/Purchase/index.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 import { Container } from "./styles";
-import { useLocation } from "react-router-dom";
+import { Button } from "../../styles/shared/Button";
 
 export default function PurchaseScreen() {
   const location = useLocation();
+  const navigateTo = useNavigate();
   console.log(location.state);
-  return <Container>PurchaseScreen</Container>;
+  return (
+    <Container>
+      <h1>Estado de la compra</h1>
+      <div className="purchaseStatusOverlay">
+        {location.state.status === "success" ? (
+          <div className="purchaseStatusContainer">
+            <h2>La compra se realizo con exito</h2>
+            <Button onClick={() => navigateTo("/")}>Volver al catalogo</Button>
+          </div>
+        ) : (
+          <div className="purchaseStatusContainer">
+            <h2>
+              Ocurrio un error con la compra, revisa que el importe no supere el
+              credito disponible en tu cuenta
+            </h2>
+            <Button width="" onClick={() => navigateTo("/cart")}>
+              Volver al carrito
+            </Button>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
 }
