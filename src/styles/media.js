4f92ceecfd152d255274a539e374494f6b06f1b3
bberos <bberos.171@gmail.com>
2022-03-09 04:38:14 -0300

    feat(medias-queries)

---
+++ src/styles/media.js
@@ -0,0 +1,12 @@
+const size = {
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "1600px",
+  desktopL: "2000px",
+};
+export const media = {
+  laptop: `@media(min-width: ${size.laptop})`,
+  laptopL: `@media(min-width: ${size.laptopL})`,
+  desktop: `@media(min-width: ${size.desktop})`,
+  desktopL: `@media(min-width: ${size.desktopL})`,
+};
